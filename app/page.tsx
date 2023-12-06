90fe3759f4ec9019ce3703356fb1f855221e2b9d
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-06 23:01:58 +0000

    input height updated

--- app/page.tsx
+++ app/page.tsx
@@ -145,14 +145,14 @@ export default function Home() {
           </div>
 
           {/* input area */}
-          <div className="flex sticky bottom-0 w-full px-6 pb-6 h-24">
-            <div className="w-full relative">
+          <div className="flex sticky bottom-0 w-full px-6 pb-6 h-20">
+            <div className="w-full relative ">
               <textarea
                 aria-label="chat input"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 placeholder="Type a message"
-                className="w-full h-full resize-none rounded-full border border-slate-900/10 bg-white pl-6 pr-24 py-[25px] text-base placeholder:text-slate-400 focus:border-violet-500 focus:outline-none focus:ring-4 focus:ring-violet-500/10 shadow-[0_10px_40px_0px_rgba(0,0,0,0.15)]"
+                className="w-full h-full resize-none rounded-full border border-slate-900/10 bg-white pl-6 pr-24 py-[14px] text-base placeholder:text-slate-400 focus:border-violet-500 focus:outline-none focus:ring-4 focus:ring-violet-500/10 shadow-[0_10px_40px_0px_rgba(0,0,0,0.15)]"
                 onKeyDown={(e) => {
                   if (e.key === 'Enter' && !e.shiftKey) {
                     e.preventDefault();
@@ -165,7 +165,7 @@ export default function Home() {
                   e.preventDefault();
                   handleClick();
                 }}
-                className="flex w-14 h-14 items-center justify-center rounded-full px-3 text-sm  bg-violet-600 font-semibold text-white hover:bg-violet-700 active:bg-violet-800 absolute right-2 bottom-2 disabled:bg-violet-100 disabled:text-violet-400"
+                className="flex w-10 h-10 items-center justify-center rounded-full px-3 text-sm  bg-violet-600 font-semibold text-white hover:bg-violet-700 active:bg-violet-800 absolute right-2 bottom-2 disabled:bg-violet-100 disabled:text-violet-400"
                 type="submit"
                 aria-label="Send"
                 disabled={!message || loading}
