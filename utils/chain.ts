fa0cde1b9b0a0289ae45efcec4b96a0da3ed6780
Tiago Pereira <tiagorp2004@gmail.com>
2023-12-07 23:28:27 +0000

    changes on chain

--- utils/chain.ts
+++ utils/chain.ts
@@ -1,13 +1,22 @@
 import { OpenAI } from 'langchain/llms/openai';
+import { pinecone } from '@/utils/pinecone-client';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { FaissStore } from 'langchain/vectorstores/faiss';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { STANDALONE_QUESTION_TEMPLATE, QA_TEMPLATE } from './prompt_template';
 
 async function initChain() {
-  const model = new OpenAI({});
-  let loaders = [new PDFLoader('chatbot-with-data/public/ro.pdf')];
+  const model = new OpenAI({
+    modelName: 'gpt-3.5-turbo',
+    verbose: true,
+    temperature: 0,
+  });
+
+  const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX ?? '');
+
+  let loaders = [new PDFLoader('/public/ro.pdf')];
   let pages = [];
   for (let loader of loaders) {
     pages.push(...(await loader.load()));
@@ -17,12 +26,32 @@ async function initChain() {
     chunkOverlap: 50,
   });
   let docs = text_splitter.splitDocuments(pages);
-  let db = await FaissStore.fromDocuments(await docs, new OpenAIEmbeddings());
-  /* create vectorstore*/
+  const documents = await docs;
 
-  return ConversationalRetrievalQAChain.fromLLM(model, db.asRetriever(), {
-    returnSourceDocuments: true,
+  // Create vector store
+  await PineconeStore.fromDocuments(documents, new OpenAIEmbeddings({}), {
+    pineconeIndex: pineconeIndex,
+    namespace: process.env.PINECONE_NAME_SPACE,
+    textKey: 'text',
   });
+  const vectorStore = await PineconeStore.fromExistingIndex(
+    new OpenAIEmbeddings({}),
+    {
+      pineconeIndex: pineconeIndex,
+      textKey: 'text',
+      namespace: process.env.PINECONE_NAME_SPACE,
+    }
+  );
+
+  return ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever(),
+    {
+      returnSourceDocuments: true,
+      qaTemplate: QA_TEMPLATE,
+      questionGeneratorTemplate: STANDALONE_QUESTION_TEMPLATE,
+    }
+  );
 }
 
 export const chain = await initChain();
